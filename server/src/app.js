fa518a33865f8ceae882fe0eed6da31dcfed2c04
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-11-01 18:48:46 +0700

    Re-structuring

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const router = require('./router');
 
 const app = express();
 
@@ -14,6 +13,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// using middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,8 +28,8 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// routers
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- server/src/router.js
+++ server/src/router.js
@@ -1,9 +1,14 @@
 const express = require('express');
+
+const usersRouter = require('./routes/users');
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/users', usersRouter);
 
 module.exports = router;
