78d4f7de5938286454408a28a7d7ce71de08e8a1
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-11-05 21:11:51 +0700

    Login and Register - change payload

--- src/helpers/error-helper.js
+++ src/helpers/error-helper.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 
 const errorHelper = (err) => {
   if (err instanceof mongoose.Error) {
-    console.log(err);
     if (err.name === 'ValidationError') err.status = 400;
   } else if (err instanceof jwt.JsonWebTokenError) err.status = 401;
   else if (err instanceof jwt.TokenExpiredError) err.status = 401;
