ab11efaaee828cedf8b9d64a3ec90fb9754e90fb
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2021-08-21 04:14:00 +0700

    New Reset

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import e, { ErrorRequestHandler, json, urlencoded } from 'express';
+import createHttpError from 'http-errors';
+import morgan from 'morgan';
+import router from './router';
+
+const app = e();
+
+if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+app.use('/', router);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createHttpError(404));
+});
+
+// error handler
+app.use(((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+}) as ErrorRequestHandler);
+
+export default app;
